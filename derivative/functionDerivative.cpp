d321a955a62c10ddb2c2a8a901a9f5817fae98e3
Dario Zivkovic <dariozivkovic0@gmail.com>
2019-07-14 20:10:10 +0200

    remove make_unique impl.

--- derivative/functionDerivative.cpp
+++ derivative/functionDerivative.cpp
@@ -2,11 +2,6 @@
 #include <math.h>
 #include <memory>
 
-//template<typename T, typename... Args>
-//std::unique_ptr<T> make_unique(Args&&... args)
-//{
-//    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-//}
 
 double cube(double aD)
 {
